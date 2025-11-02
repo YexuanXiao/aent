5c968b4e30ef8c404f421d4f2dfff145bab049af
YexuanXiao <bizwen@nykz.org>
2025-11-02 15:20:15 +0800

    Reorder include statements in trap.cpp

--- trap_program/trap.cpp
+++ trap_program/trap.cpp
@@ -1,7 +1,7 @@
 #pragma comment(linker, "/subsystem:windows")
 
-#include <errhandlingapi.h>
 #include <windows.h>
+#include <errhandlingapi.h>
 
 int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine,
                     _In_ int nShowCmd)
